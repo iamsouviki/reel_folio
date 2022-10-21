5cf0bce405271266faa8d88304c808995d8985f5
ATRI DAS <babinsamrat123@gmail.com>
2022-10-22 01:03:10 +0530

    makes some changes

--- lib/route/reel_folio_route.dart
+++ lib/route/reel_folio_route.dart
@@ -10,34 +10,35 @@ abstract class ReelFolioRoute {
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     currentRouteName = settings.name;
-    Widget _currentWidget;
+    Widget currentWidget;
 
     switch (settings.name) {
+      case RoutePath.routeInitial:
       case RoutePath.routeToRequestOnBoardingScreen:
-        _currentWidget = OnBoardingRequestScreen();
+        currentWidget = OnBoardingRequestScreen();
         break;
 
       case RoutePath.routeToOnBoardingDetailsScreen:
-        _currentWidget = OnBoardingDetailsScreen();
+        currentWidget = OnBoardingDetailsScreen();
         break;
 
       case RoutePath.routeToOnBoardingConfirmationScreen:
-        _currentWidget = const OnBoardingRequestConfirmationScreen();
+        currentWidget = const OnBoardingRequestConfirmationScreen();
         break;
 
       case RoutePath.routeToWelcomeScreen:
-        _currentWidget = const WelcomeScreen();
+        currentWidget = const WelcomeScreen();
         break;
 
       default:
-        _currentWidget = Scaffold(
+        currentWidget = Scaffold(
           body: Center(
             child: Text('No route defined for ${settings.name}'),
           ),
         );
     }
 
-    return _ReelFolioRoute(screen: _currentWidget, settings: settings);
+    return _ReelFolioRoute(screen: currentWidget, settings: settings);
   }
 }
 
--- lib/route/route_path.dart
+++ lib/route/route_path.dart
@@ -1,5 +1,6 @@
 abstract class RoutePath {
-  static const routeToRequestOnBoardingScreen = '/'; //todo: will be changed
+  static const routeInitial = '/';
+  static const routeToRequestOnBoardingScreen = 'onboarding-request-screen'; //todo: will be changed
   static const routeToOnBoardingDetailsScreen= 'onboarding-details-screen';
   static const routeToOnBoardingConfirmationScreen = 'onboarding-confirmation-screen';
   static const routeToWelcomeScreen = 'welcome-screen';
