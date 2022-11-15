d258ab49d0c1c98c7d4276e6b27ff3773b671b2b
dartcube-dev <dartcube.io@gmail.com>
2022-11-15 23:18:54 +0530

    makes some changes

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,6 @@ class ReelFolioApp extends StatelessWidget {
             SizeConfig().init(constraints, orientation);
             return const MaterialApp(
               title: 'Reel Folio',
-              //theme: ThemeData(fontFamily: 'Sora'),
               debugShowCheckedModeBanner: false,
               // onGenerateRoute: ReelFolioRoute.generateRoute,
               home: SplashScreen(),
