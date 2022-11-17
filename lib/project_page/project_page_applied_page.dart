8b515c83a11b093e5a97dfc9800918604fa13442
shubhayu-64 <majumdarshubhayu@gmail.com>
2022-11-17 15:36:35 +0530

    Fixed issues

--- lib/project_page/project_page_applied_page.dart
+++ lib/project_page/project_page_applied_page.dart
@@ -37,11 +37,8 @@ class _ProjectAppliedPageState extends State<ProjectAppliedPage> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Padding(
-                padding: EdgeInsets.only(top: screenWidth! * 0.05),
-                child: ProjectPageAppbarWidget(
-                  title: "Shoot in 30 Days",
-                ),
+              ProjectPageAppbarWidget(
+                title: "Shoot in 30 Days",
               ),
               Padding(
                 padding: EdgeInsets.symmetric(
@@ -135,6 +132,134 @@ class _ProjectAppliedPageState extends State<ProjectAppliedPage> {
                     ),
                   ),
                 ],
+              ),
+              Divider(
+                color: Colors.white.withOpacity(0.4),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: screenWidth! * 0.05,
+                    vertical: screenWidth! * 0.03),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Location",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    Text(
+                      "Los Angeles, CA",
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.6),
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Divider(
+                color: Colors.white.withOpacity(0.4),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: screenWidth! * 0.05),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: screenWidth! * 0.02),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Comments",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 15),
+                          child:
+                              Image.asset(ReelfolioImageAsset.profilePicture),
+                        ),
+                        Text(
+                          "@saffod",
+                          style: TextStyle(
+                            color: ReelfolioColor.usernameColor,
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        Text(
+                          " This looks so cool!",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "View all Comments..",
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.6),
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Image.asset(ReelfolioImageAsset.profilePicture),
+                        // TextField(
+                        //   decoration: InputDecoration(
+                        //     border: OutlineInputBorder(),
+                        //     labelText: 'Add a comment as @jonsey',
+                        //   ),
+                        //   style: ,
+                        // ),
+                        Container(
+                            width: screenWidth! * 0.8   ,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: screenWidth! * 10 / 375,
+                                vertical: 10),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                contentPadding:
+                                    EdgeInsets.symmetric(vertical: 10.0),
+                                filled: true,
+                                hintText: "  Add a comment as @jonsey",
+                                hintStyle: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.white.withOpacity(0.6),
+                                ),
+                                fillColor: ReelfolioColor.bgColor,
+                                border: OutlineInputBorder(
+                                    borderSide: BorderSide.none,
+                                    borderRadius: BorderRadius.circular(10)),
+                              ),
+                            )),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
